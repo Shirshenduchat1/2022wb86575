22f8bcb6dd1288509edd1f14c4e13e0b4822e9b7
Shirshendu Chatterjee <2022wb86575@wilp.bits-pilani.ac.in>
2026-09-21 04:22:19 +0530

    Modified source code for CI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI");
+        System.out.println("Hello from Jenkins Continuous Integration - Version 2");
     }
 }
